07b19cd5b7b6b664fc45ccbd867babd8bb6b59a8
Hassen Rmili <hassen.rmili.hr4@gmail.com>
2023-10-16 01:18:44 +0100

    #ADD: Guessing Game

--- chapter02/main.cpp
+++ chapter02/main.cpp
@@ -2,7 +2,7 @@
  * @Author: Hassen Rmili
  * @Date:   2023-10-15 03:11:21
  * @Last Modified by:   Hassen Rmili
- * @Last Modified time: 2023-10-16 00:32:26
+ * @Last Modified time: 2023-10-16 01:17:55
  */
 
 #include <iostream>
@@ -12,13 +12,20 @@
 
 int main()
 {
-  unsigned int guess;
-  unsigned char number;
+  //? Seeding
+  srand(time(NULL));
+  unsigned int guess = rand() % 100;
 
-  std::cout << "Guess the number" << std::endl;
+  unsigned int number;
+
+  std::cout << "Guess the number between 0 and 99" << std::endl;
   std::cin >> number;
 
-  std::cout << number << std::endl;
- 
+  std::cout << "You guessed: "
+            << number
+            << ", The number was "
+            << guess
+            << std::endl;
+
   return 0;
 }

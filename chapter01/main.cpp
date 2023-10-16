e8c565d6067d8dc990772210148da643e5b98ec4
Hassen Rmili <hassen.rmili.hr4@gmail.com>
2023-10-16 01:05:34 +0100

    #ADD: Chapter02

---
+++ chapter01/main.cpp
@@ -0,0 +1,19 @@
+/**
+ * @Author: Hassen Rmili
+ * @Date:   2023-10-15 02:55:03
+ * @Last Modified by:   Hassen Rmili
+ * @Last Modified time: 2023-10-16 00:02:16
+ */
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+  std::string name;
+  std::cout << "Whaat is your name : ";
+  std::cin >> name;
+  std::cout << "Your name is " << name << std::endl;
+
+  return 0;
+}

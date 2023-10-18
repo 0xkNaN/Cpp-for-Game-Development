3da119fc2029b5695760b7335e5a2f8ce799d96b
Hassen Rmili <hassen.rmili.hr4@gmail.com>
2023-10-18 12:48:30 +0100

    #ADD: Chapter05

--- game/main.cpp
+++ game/main.cpp
@@ -2,13 +2,31 @@
  * @Author: Hassen Rmili
  * @Date:   2023-10-17 21:47:40
  * @Last Modified by:   Hassen Rmili
- * @Last Modified time: 2023-10-17 21:47:49
+ * @Last Modified time: 2023-10-18 12:47:54
  */
 
 #include <iostream>
+#include <string.h>
+
+struct Player
+{
+  std::string m_name;
+};
+
+void welcomePlayer(Player &player)
+{
+
+  std::cout << "Welcome to Text Adventure!" << std::endl;
+  std::cout << std::endl;
+
+  std::cout << "What is your name?" << std::endl;
+  std::getline(std::cin, player.m_name);
+}
 
 int main(int argc, char const *argv[])
 {
-  /* code */
+  Player player;
+  welcomePlayer(player);
+
   return 0;
 }
